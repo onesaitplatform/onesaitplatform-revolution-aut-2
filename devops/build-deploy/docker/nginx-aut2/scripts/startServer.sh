9ade2a61e7f3a39222a72f309c6a1103e6db65e5
Jordi Paraire <jjparaire@minsait.com>
2019-10-03 17:50:32 +0200

    Adding gunicorn as djsango wsgi server.

--- devops/build-deploy/docker/nginx-aut2/scripts/startServer.sh
+++ devops/build-deploy/docker/nginx-aut2/scripts/startServer.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-echo "Starting nginx"
+echo "              _                                        __ ";
+echo "  _ __   __ _(_)_ __ __  __  _ __ ___  ___ ___  _ __  / _|";
+echo " | '_ \ / _\` | | '_ \\ \/ / | '__/ _ \/ __/ _ \| '_ \| |_ ";
+echo " | | | | (_| | | | | |>  <  | | |  __/ (_| (_) | | | |  _|";
+echo " |_| |_|\__, |_|_| |_/_/\_\ |_|  \___|\___\___/|_| |_|_|  ";
+echo " | |__  |___/     / \  _   _| |_    |___ \                ";
+echo " | '_ \| | | |   / _ \| | | | __|____ __) |               ";
+echo " | |_) | |_| |  / ___ \ |_| | ||_____/ __/                ";
+echo " |_.__/ \__, | /_/   \_\__,_|\__|   |_____|               ";
+echo "        |___/                                             ";
+echo "{petrigilf,kristof.pap,paraire}@gmail.com"
+echo ""
+
+echo "nginx version"
 /usr/sbin/nginx -V
+
+echo ""
+
+echo "Starting nginx"
 /usr/sbin/nginx -c /etc/nginx/nginx.conf
 
-echo "Starting django"
-python /app/manage.py runserver 0.0.0.0:8000
+echo "Starting Gunicorn"
+gunicorn -b0.0.0.0:8000 reconf.wsgi
 
